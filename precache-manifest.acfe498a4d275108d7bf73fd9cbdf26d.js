deaa1906fca3c5b68651c95f971c8c4e2d29d8d6
Tyler Ruth <tyler1hero@sbcglobal.net>
2020-12-05 15:39:52 -0500

    Updates

---
+++ precache-manifest.acfe498a4d275108d7bf73fd9cbdf26d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a6159c4cc86b3cd13eea006006944fd",
+    "url": "/NASA-Today/index.html"
+  },
+  {
+    "revision": "60cae70d42237cd1a89a",
+    "url": "/NASA-Today/static/css/main.2f774b28.chunk.css"
+  },
+  {
+    "revision": "817daf19981584c7586e",
+    "url": "/NASA-Today/static/js/2.db5557d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/NASA-Today/static/js/2.db5557d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60cae70d42237cd1a89a",
+    "url": "/NASA-Today/static/js/main.222342db.chunk.js"
+  },
+  {
+    "revision": "7ad8f854cf4c15332a75",
+    "url": "/NASA-Today/static/js/runtime-main.695ec042.js"
+  },
+  {
+    "revision": "6099842fdaa23fbcaa3260f73c528545",
+    "url": "/NASA-Today/static/media/nasa-logo.6099842f.png"
+  }
+]);
